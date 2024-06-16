6692b4c05f56172bb3347517e9f641ddedd6a032
Manohar_nalluri <manoharnalluri47@gmail.com>
2024-06-16 14:37:35 +0530

    checking

--- activityTracker/src/Api/axios.jsx
+++ activityTracker/src/Api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:"https://lobster-app-jx83i.ondigitalocean.app:8080/api/v1/"
+  baseURL:"https://lobster-app-jx83i.ondigitalocean.app/api/v1/"
 })
