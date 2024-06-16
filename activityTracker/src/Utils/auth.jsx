38032cec7ad894d3759cb158cffc82498ade8461
Manohar_nalluri <manoharnalluri47@gmail.com>
2024-06-16 14:45:10 +0530

    cookie

--- activityTracker/src/Utils/auth.jsx
+++ activityTracker/src/Utils/auth.jsx
@@ -1,8 +1,10 @@
 import Cookies from 'js-cookie';
+import { document } from 'postcss';
 
 const useAuth = () => {
   const token = Cookies.get('accessToken');
   console.log(token)
+  console.log(document.cookie)
   return !!token; 
 };
 
